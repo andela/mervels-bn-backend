842fbcf94b1377593b20dc7a441669520e04dc38
Jonathan Shyaka <jonashyaka2@gmail.com>
2019-09-04 21:28:40 +0200

    chore(sequelize): setup the api to use sequelize as orm (#9)
    [Finishes #168131401]

---
+++ src/database/config.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+module.exports = {
+  production: {
+    use_env_variable: process.env.DATABASE_URL,
+    dialect: 'postgres',
+  },
+  development: {
+    use_env_variable: process.env.DEV_DATABASE_URL,
+    dialect: 'postgres',
+  },
+  test: {
+    use_env_variable: process.env.DATABASE_URL_TEST,
+    dialect: 'postgres',
+  },
+};
---
+++ src/database/models/user.js
@@ -0,0 +1,11 @@
+'use strict';
+
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    example: DataTypes.STRING
+  }, {});
+  User.associate = function (models) {
+    // associations can be defined here
+  };
+  return User;
+};
