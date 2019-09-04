3e861419b29251eb7f70e62d8824ba3232bcc6e3
Bahatiroben <39488016+Bahatiroben@users.noreply.github.com>
2019-09-04 19:58:54 +0200

    chore(unit-tests): setup unit tests (#11)
    - all implementation are tested
    - this integrates with travis-ci for continuous integration
    
    [Finishes #168131405]

---
+++ src/test/server.test.js
@@ -0,0 +1,23 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+
+import app from '../index';
+
+chai.should();
+chai.use(chaiHttp);
+
+describe('server test', () => {
+  it('should handle the unknown routes', (done) => {
+    chai
+      .request(app)
+      .get('/unknownroute')
+      .send({ data: 'nothing' })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.status.should.equal(404);
+        return done();
+      });
+  });
+});
