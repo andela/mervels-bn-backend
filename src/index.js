f4715956ba0432c04dec3511915d796b5264c5d1
BAHATI Robben <lux@BAHATIs-MacBook-Pro.local>
2019-09-04 10:47:42 +0200

    chore(unit-tests): setup unit tests
    - all implementation are tested
    - this integrates with travis-ci for continuous integration
    
    [Starts #168131405]

--- src/index.js
+++ src/index.js
@@ -85,3 +85,5 @@ app.use((err, req, res, next) => {
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on port ${server.address().port}`);
 });
+
+export default app;
