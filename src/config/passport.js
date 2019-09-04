b32da28400a354c9296983b8b41f752839e01809
Davis Kabiswa <daviskabiswa@Daviss-MacBook-Pro.local>
2019-09-04 13:04:21 +0200

    chore(code-coverage): Integrate coveralls code coverage
     [Starts #168131407]

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,15 +1,14 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
-
 passport.use(
   new LocalStrategy(
     {
       usernameField: 'user[email]',
       passwordField: 'user[password]'
     },
-    ((email, password, done) => {
-      //Query user model to check if password valid user or password
+    (email, password, done) => {
+      // Query user model to check if password valid user or password
     }
   )
-))
+);
--- src/database/config.js
+++ src/database/config.js
@@ -4,14 +4,14 @@ dotenv.config();
 module.exports = {
   production: {
     use_env_variable: process.env.DATABASE_URL,
-    dialect: 'postgres',
+    dialect: 'postgres'
   },
   development: {
     use_env_variable: process.env.DEV_DATABASE_URL,
-    dialect: 'postgres',
+    dialect: 'postgres'
   },
   test: {
     use_env_variable: process.env.DATABASE_URL_TEST,
-    dialect: 'postgres',
-  },
+    dialect: 'postgres'
+  }
 };
